8b5ec57d9daaac374cfbadf7634b877008483a9b
Henrique <henriquepinheiroxavier@gmail.com>
2024-07-03 11:13:24 -0300

    feat: start new project with an initial configuration

---
+++ src/pages/Login.tsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+
+const LoginPageContainer = styled.div`
+    display: flex;
+    overflow: hidden;
+    height: 100vh;
+`
+const FormContainer = styled.div`
+    width: 40%;
+`
+const PhotosSlider = styled.div`
+    width: 60%;
+`
+
+const Login = () => {
+    return (
+        <LoginPageContainer>
+            <FormContainer>
+              
+            </FormContainer>
+            <PhotosSlider>
+                
+            </PhotosSlider>
+    </LoginPageContainer>
+  )
+}
+
+export default Login
