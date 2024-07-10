e0379fa080ab03789f83d552b36a92fe5d0a2fb3
Henrique <henriquepinheiroxavier@gmail.com>
2024-07-10 18:07:30 -0300

    fix error in deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { store } from './redux/store.ts';
@@ -6,8 +5,6 @@ import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <React.StrictMode>
       <App />
-    </React.StrictMode>
   </Provider >
 )
