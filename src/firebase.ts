4694dd6959e9f76d221cc03dbacbae83f7e9e267
Henrique <henriquepinheiroxavier@gmail.com>
2024-07-12 21:29:04 -0300

    feat: update firebase keys

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,11 +2,12 @@ import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "codeduo-4f967.firebaseapp.com",
-  projectId: "codeduo-4f967",
-  storageBucket: "codeduo-4f967.appspot.com",
-  messagingSenderId: "972882557852",
-  appId: "1:972882557852:web:e7cd7171b905dba3b4e659"
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
